f9a18d9470ebb98bf4c2d7713c3887d357a7f462
anuwat-an <anuwat.an@ku.th>
2017-09-14 19:25:54 +0700

    repeated appt, change db handling, class diagram, unit test

--- src/test/java/CalendarTest.java
+++ src/test/java/CalendarTest.java
@@ -11,10 +11,10 @@ import java.time.LocalDateTime;
 
 public class CalendarTest {
 
-    private Calendar calendar;
+    private static Calendar calendar;
 
     @BeforeAll
-    public void setUp() {
+    public static void setUp() {
         calendar = new Calendar();
 
         LocalDateTime lc = LocalDateTime.of(2017, 9, 13, 20, 48);
@@ -27,10 +27,20 @@ public class CalendarTest {
     @Test
     public void testGetAppointments() {
         assertEquals(4, calendar.getAppointments().size());
-        assertEquals("20: WEDNESDAY 13/09/2017 20:48\nJUnit\ntest #1\n", calendar.getAppointment(20));
-        assertEquals("21: DAILY 20:48\nJUnit\ntest #2\n", calendar.getAppointment(21));
-        assertEquals("22: WEDNESDAY 20:48\nJUnit\ntest #3\n", calendar.getAppointment(22));
-        assertEquals("23: MONTHLY 13 20:48\nJUnit\ntest #4\n", calendar.getAppointment(23));
+        assertEquals("20: WEDNESDAY 13/9/2017 20:48\nJUnit\ntest #1\n", calendar.getAppointment(20).toString());
+        assertEquals("21: DAILY 20:48\nJUnit\ntest #2\n", calendar.getAppointment(21).toString());
+        assertEquals("22: WEDNESDAY 20:48\nJUnit\ntest #3\n", calendar.getAppointment(22).toString());
+        assertEquals("23: MONTHLY 13 20:48\nJUnit\ntest #4\n", calendar.getAppointment(23).toString());
+    }
+
+    @Test
+    public void testDeleteAppointment() {
+        calendar.deleteAppointment(22);
+
+        assertEquals(3, calendar.getAppointments().size());
+        assertEquals("20: WEDNESDAY 13/9/2017 20:48\nJUnit\ntest #1\n", calendar.getAppointment(20).toString());
+        assertEquals("21: DAILY 20:48\nJUnit\ntest #2\n", calendar.getAppointment(21).toString());
+        assertEquals("23: MONTHLY 13 20:48\nJUnit\ntest #4\n", calendar.getAppointment(23).toString());
     }
 
 }
